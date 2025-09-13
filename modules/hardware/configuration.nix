8a8d77adb1f86c05f0205e9cde23bf747f563e9a
monke <monke@monkle.com>
2025-09-13 16:29:36 +1200

    Sat 13 Sep 2025 04:29:36 PM

--- modules/hardware/configuration.nix
+++ modules/hardware/configuration.nix
@@ -6,9 +6,12 @@
 
 {
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
+    timeoutStyle = "hidden";
+  };
 
   networking.hostName = "server"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
